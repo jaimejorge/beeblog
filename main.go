8a8c269639bdcc2b1e052ebdf8a58562c6ecda27
Lei Cao <lei.cao@zalora.com>
2014-11-15 15:38:09 +0800

    the generated beeblog api

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	_ "beeblog/docs"
+	_ "beeblog/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/beeblog")
+}
+
+func main() {
+	if beego.RunMode == "dev" {
+		beego.DirectoryIndex = true
+		beego.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
---
+++ routers/beeblog_controllers_commentsRouter.go
@@ -0,0 +1,114 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	
+	beego.GlobalControllerRouter["beeblog/controllers:CommentsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:CommentsController"],
+		beego.ControllerComments{
+			"Post",
+			`/`,
+			[]string{"post"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:CommentsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:CommentsController"],
+		beego.ControllerComments{
+			"GetOne",
+			`/:id`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:CommentsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:CommentsController"],
+		beego.ControllerComments{
+			"GetAll",
+			`/`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:CommentsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:CommentsController"],
+		beego.ControllerComments{
+			"Put",
+			`/:id`,
+			[]string{"put"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:CommentsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:CommentsController"],
+		beego.ControllerComments{
+			"Delete",
+			`/:id`,
+			[]string{"delete"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:PostsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:PostsController"],
+		beego.ControllerComments{
+			"Post",
+			`/`,
+			[]string{"post"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:PostsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:PostsController"],
+		beego.ControllerComments{
+			"GetOne",
+			`/:id`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:PostsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:PostsController"],
+		beego.ControllerComments{
+			"GetAll",
+			`/`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:PostsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:PostsController"],
+		beego.ControllerComments{
+			"Put",
+			`/:id`,
+			[]string{"put"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:PostsController"] = append(beego.GlobalControllerRouter["beeblog/controllers:PostsController"],
+		beego.ControllerComments{
+			"Delete",
+			`/:id`,
+			[]string{"delete"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:UsersController"] = append(beego.GlobalControllerRouter["beeblog/controllers:UsersController"],
+		beego.ControllerComments{
+			"Post",
+			`/`,
+			[]string{"post"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:UsersController"] = append(beego.GlobalControllerRouter["beeblog/controllers:UsersController"],
+		beego.ControllerComments{
+			"GetOne",
+			`/:id`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:UsersController"] = append(beego.GlobalControllerRouter["beeblog/controllers:UsersController"],
+		beego.ControllerComments{
+			"GetAll",
+			`/`,
+			[]string{"get"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:UsersController"] = append(beego.GlobalControllerRouter["beeblog/controllers:UsersController"],
+		beego.ControllerComments{
+			"Put",
+			`/:id`,
+			[]string{"put"},
+			nil})
+
+	beego.GlobalControllerRouter["beeblog/controllers:UsersController"] = append(beego.GlobalControllerRouter["beeblog/controllers:UsersController"],
+		beego.ControllerComments{
+			"Delete",
+			`/:id`,
+			[]string{"delete"},
+			nil})
+
+}
